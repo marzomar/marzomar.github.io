e22cf2ffd6cae648204c05e040ed0ca7c0ed10bf
omar marzougui <marzouguiomar@hotmail.fr>
2019-01-06 15:43:18 +0100

    Eventually last commit

--- js/resume.js
+++ js/resume.js
@@ -15,7 +15,7 @@
     }
   });
  */
-  // Closes responsive menu when a scroll trigger link is clicked
+  // Ferme le menu quand on clique sur un lien
     $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
