36d3704d36895fffaad283af621cb3377fc5d078
omar marzougui <marzouguiomar@hotmail.fr>
2019-01-10 14:34:52 +0100

    Test video logo et background

--- js/resume.js
+++ js/resume.js
@@ -5,3 +5,17 @@
     $('.navbar-collapse').collapse('hide');
   });
 })(jQuery); // End of use strict
+
+var v = document.getElementById("video");
+var p = document.getElementById('image')
+
+/* v.addEventListener('play', function(){
+  document.getElementsByTagName("body").style.overflow = 'hidden';
+}) */
+
+v.addEventListener('ended', function(){
+  v.style.display = 'none';
+  v.load(); 
+}, false);
+
+
